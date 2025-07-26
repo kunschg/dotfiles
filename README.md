f5a9547c3eca1d643c4f475dc8e7b9566a3fc86e
kunschg <kunschguillaume@gmail.com>
2025-07-26 19:11:05 +0200

    v0

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dotfiles
+
+To run and install, do:
+git clone <https://github.com/kunschg/dotfiles.git>
+
+cd dotfiles
+install
+bootstrap.sh
---
+++ ble/install.sh
@@ -0,0 +1,2 @@
+curl -L https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf -
+bash ble-nightly/ble.sh --install ~/.local/share
